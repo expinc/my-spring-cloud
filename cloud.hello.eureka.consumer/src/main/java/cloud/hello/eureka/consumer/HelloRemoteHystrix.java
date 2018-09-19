d181e3747a5782e9eeb37bdaf93d350ae4977385
expinc <expinc@outlook.com>
2018-09-19 18:15:45 +0800

    Add Hystrix fusing

---
+++ cloud.hello.eureka.consumer/src/main/java/cloud/hello/eureka/consumer/HelloRemoteHystrix.java
@@ -0,0 +1,17 @@
+package cloud.hello.eureka.consumer;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Component
+public class HelloRemoteHystrix implements HelloRemote
+{
+
+	@Override
+	public String hello(@RequestParam(value = "name") String name)
+	{
+		return "Hello fallback";
+	}
+
+}
